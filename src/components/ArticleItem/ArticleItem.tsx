382d7bc70c84d0ca4f674e69809f352779ef7ebe
Berkan Unal <Berkanunal@gmail.com>
2019-11-24 20:18:12 +0300

    Add bunch of dummy data to article component,
    * Add todos
    * Temporarily commnet out prop interface

--- src/components/ArticleItem/ArticleItem.tsx
+++ src/components/ArticleItem/ArticleItem.tsx
@@ -3,13 +3,77 @@ import "./ArticleItem.scss";
 import React from "react";
 
 interface ArticleItemProps {
-  article: string;
+  /* article: {
+    title: string;
+    dateTime?: string;
+  }; */
+  article: any;
 }
 
-function ArticleItem({article}: ArticleItemProps) {
+function ArticleItem({article: {title, dateTime}}: ArticleItemProps) {
+  // TODO: convert displayed datetime to relative dates like: 1 hour ago, etc.
+  // TODO: wrap <time> elements with a component.
+  // - it should take a display format prop like: displayFormat={dd-mm-yyyy}
+  // TODO: source span should link to /source/:source, ex: /source/reddit
+  // - at sourcePage, we should display a link to actual source like: reddit.com
+  // TODO: article-popularity should be a component with rounding logic
+  // TODO: use https://schema.org/Article for better SEO
+  // TODO: add favicon of source to source element
+
+  // TODO: @high fix all a elements. They should be router links, or external links
+
   return (
     <li className={"article-item"}>
-      <p>{article}</p>
+      <div className={"article-popularity-wrapper"}>
+        <span className={"fire-emoji"}>{"👁"}</span>
+
+        <span className={"article-popularity"}>{"2k"}</span>
+      </div>
+
+      <article className={"article"}>
+        <header className={"article-title-wrapper"}>
+          <h1 className={"article-title"}>
+            <a href="/domain/github.io/">{title}</a>
+          </h1>
+
+          <h2 className={"article-meta"}>
+            <a
+              className={"article-topic"}
+              href={"/topic/topicname/"}
+            >
+              {"Topic"}
+            </a>
+
+            <p className={"article-source"}>
+              {"at "}
+
+              <a href={"/source/source-name/"}>{"Source.io"}</a>
+            </p>
+
+            <a
+              className={"article-author"}
+              href={"/author/author-name/"}
+            >
+              {"by Author Authorinio"}
+            </a>
+          </h2>
+        </header>
+
+        <p className="article-teaser-preview">
+          {"This will be small teaser of the content. It will be 130 characters."}
+          {"It should show elipsis at the end of teaser. 10 extra chrcters"}
+        </p>
+
+        <footer className="article-footer">
+          <p className="article-publish-date">
+            {"published "}
+
+            <time dateTime={dateTime || "2020"}>{"2 months ago"}</time>
+          </p>
+
+          <p className="article-reading-time">{"3 minutes read"}</p>
+        </footer>
+      </article>
     </li>
   );
 }
