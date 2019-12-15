4debd0afa7e8de4b8b7c06d5f0ec8bd232e948d1
Berkan Unal <Berkanunal@gmail.com>
2019-12-15 17:48:58 +0300

    Add skeleton of input component, add classnames to packages

---
+++ src/components/Inputs/Input/Input.tsx
@@ -0,0 +1,84 @@
+import "./Header.scss";
+
+import React from "react";
+import classNames from "classnames";
+
+import {KEYBOARD_EVENT_KEY} from "../../../utils/event-constants";
+import {TInputType} from "../../../utils/input-constants";
+
+interface InputProps {
+  id: string;
+  className?: string;
+  name: string;
+  title?: string;
+  type?: TInputType;
+  value?: string;
+  onChange: React.ReactEventHandler<HTMLInputElement>;
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  placeholder?: string;
+  errorMessage?: string;
+  description?: React.ReactNode;
+  isDisabled?: boolean;
+  autoFocus?: boolean;
+}
+
+export function Input({
+  id,
+  className,
+  name,
+  title,
+  type = "text",
+  value,
+  onChange,
+  onKeyDown,
+  isDisabled,
+}: InputProps) {
+  const wrapperClassName = classNames(className, "input-wrapper");
+
+  return (
+    <div
+      id={id}
+      className={wrapperClassName}
+    >
+      {title && (
+        <label
+          id={`${id}-label`}
+          htmlFor={id}
+          className={"input-label"}
+        >
+          {title}
+        </label>
+      )}
+
+      <input
+        id={`${id}-input`}
+        className={"input"}
+        type={type}
+        name={name}
+        disabled={isDisabled}
+        onKeyDown={handleKeyDown}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === KEYBOARD_EVENT_KEY.ESCAPE) {
+      const cleanEvent = clearValueOfEvent(event);
+
+      onChange(cleanEvent);
+    }
+
+    /* if (onKeyDown) {
+      onKeyDown(event);
+    } */
+
+    function clearValueOfEvent(event: React.KeyboardEvent<HTMLInputElement>) {
+      const cleanValueEvent = {...event};
+      cleanValueEvent.currentTarget.value = "";
+
+      return cleanValueEvent;
+    }
+  }
+}
---
+++ src/utils/event-constants.ts
@@ -0,0 +1,11 @@
+export const KEYBOARD_EVENT_KEY = {
+  ENTER: "Enter",
+  SPACE: " ",
+  ESCAPE: "Escape",
+  TAB: "Tab",
+  BACKSPACE: "Backspace",
+  ARROW_UP: "ArrowUp",
+  ARROW_DOWN: "ArrowDown",
+  ARROW_LEFT: "ArrowLeft",
+  ARROW_RIGHT: "ArrowRight",
+};
